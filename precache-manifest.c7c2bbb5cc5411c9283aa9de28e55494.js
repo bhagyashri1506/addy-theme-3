908a82cbec323c3c91905e44deb4dd6681192fca
Harshit <harshitupadhyay872@gmail.com>
2023-05-18 13:17:04 +0530

    Updates

---
+++ precache-manifest.c7c2bbb5cc5411c9283aa9de28e55494.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf8e981ad458a5f85d36",
+    "url": "/addy-theme-3/static/css/main.de075d46.chunk.css"
+  },
+  {
+    "revision": "cf8e981ad458a5f85d36",
+    "url": "/addy-theme-3/static/js/main.cf8e981a.chunk.js"
+  },
+  {
+    "revision": "6d8f048ca440f6c8cad0",
+    "url": "/addy-theme-3/static/js/1.6d8f048c.chunk.js"
+  },
+  {
+    "revision": "b3904e325f0f25b587eb",
+    "url": "/addy-theme-3/static/css/2.214a976d.chunk.css"
+  },
+  {
+    "revision": "b3904e325f0f25b587eb",
+    "url": "/addy-theme-3/static/js/2.b3904e32.chunk.js"
+  },
+  {
+    "revision": "a109073c13361dffc7aa",
+    "url": "/addy-theme-3/static/js/runtime~main.a109073c.js"
+  },
+  {
+    "revision": "559af62b4556ca1c3a4696db3306d8a1",
+    "url": "/addy-theme-3/static/media/home.559af62b.svg"
+  },
+  {
+    "revision": "1c311785dc8ed0c4e597c899fb924777",
+    "url": "/addy-theme-3/static/media/deposit.1c311785.svg"
+  },
+  {
+    "revision": "d332005cf9bac1a4f34a49cd25ec2f39",
+    "url": "/addy-theme-3/static/media/livechat.d332005c.svg"
+  },
+  {
+    "revision": "4dcb3654299891b42846a719e7b47423",
+    "url": "/addy-theme-3/static/media/promo.4dcb3654.svg"
+  },
+  {
+    "revision": "6ed9a61b9f4881d65878ae64bbc47652",
+    "url": "/addy-theme-3/static/media/history.6ed9a61b.svg"
+  },
+  {
+    "revision": "7d2ac630503a2956eff801892bbf670e",
+    "url": "/addy-theme-3/static/media/logo.7d2ac630.svg"
+  },
+  {
+    "revision": "a4c88ad765b83a4cc3fa2c3b59aa1867",
+    "url": "/addy-theme-3/static/media/marquee.a4c88ad7.jpg"
+  },
+  {
+    "revision": "ab665c170c7e2f1248ebf5b3a3461b64",
+    "url": "/addy-theme-3/static/media/ios.ab665c17.jpg"
+  },
+  {
+    "revision": "ae0bfa82f983cf1832c7038bac0fb9c5",
+    "url": "/addy-theme-3/static/media/auto-ewallet.ae0bfa82.jpg"
+  },
+  {
+    "revision": "a2465140fa88edf31673a01e47788710",
+    "url": "/addy-theme-3/static/media/online.a2465140.jpg"
+  },
+  {
+    "revision": "4b0662c4c4bd847e9aca903917f387fa",
+    "url": "/addy-theme-3/static/media/ATM.4b0662c4.jpg"
+  },
+  {
+    "revision": "12a2ac3da87c95d4d1efd8e90be20da3",
+    "url": "/addy-theme-3/index.html"
+  }
+];
